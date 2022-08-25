d257599f72ffb8c3701db099b4c21b89cc2f5e17
DomingosOctavio <tavotere@hotmail.com>
2022-08-25 01:30:05 -0300

    primeiro commit

---
+++ ESP32_cod_.ino
@@ -0,0 +1,441 @@
+#include <NTPClient.h>
+#include <ArduinoUniqueID.h>
+#include <ArduinoJson.h>
+#include <SPI.h>
+#include <WiFiManager.h>
+#include <DNSServer.h>
+#include <Wifi.h>
+#include <HTTPClient.h>
+#include <Ethernet.h>
+#include <WiFiUDP.h>//Biblioteca do UDP.
+#include <WiFiUdp.h>
+
+
+const char* ssid = "Claro local";
+const char* password = "RJ150884";
+const char* host = "www.projetopainelsolar.tech";
+
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+EthernetClient client;
+
+
+float tensao;
+float corrente;
+float leituraLDR;
+String horario = "";
+String ip_atual;
+String Unique_Id = "";
+String formattedDate;
+String data_atual;
+int valorpot = 0; //Armazena valor lido do LDR, entre 0 e 1023 
+
+ //tensao
+float tensaoEntrada = 0.0; //VARIÁVEL PARA ARMAZENAR O VALOR DE TENSÃO DE ENTRADA DO SENSOR
+float tensaoMedida = 0.0; //VARIÁVEL PARA ARMAZENAR O VALOR DA TENSÃO MEDIDA PELO SENSOR
+float valorR1 = 30000.0; //VALOR DO RESISTOR 1 DO DIVISOR DE TENSÃO
+float valorR2 = 7500.0; // VALOR DO RESISTOR 2 DO DIVISOR DE TENSÃO
+int leituraSensor = 0; //VARIÁVEL PARA ARMAZENAR A LEITURA DO PINO ANALÓGICO
+
+WiFiServer server(80);
+
+const int pinoSensorLDR = 35; //PINO DIGITAL UTILIZADO PELA SAÍDA DO SENSOR LDR
+const int pinoSensorTensao = 34; //PINO DIGITAL UTILIZADO PELA SAÍDA DO SENSOR TENSAO
+
+
+void setup() //------------------------------------SETUP-------------------------------------
+{
+  Serial.begin(115200);
+
+  //pino sensor luminosidade
+  pinMode (pinoSensorLDR, INPUT); //DEFINE O PINO COMO ENTRADA
+  //pino sensor  TENSAO
+  pinMode(pinoSensorTensao, INPUT); //DEFINE O PINO COMO ENTRADA
+
+  Serial.println();
+  Serial.print("Conectando-se a ");
+  Serial.println(ssid);
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectada.");
+  Serial.println("Endereço de IP: ");
+  Serial.println(WiFi.localIP());
+
+  server.begin();
+  // Initialize a NTPClient to get time
+  timeClient.begin();
+  timeClient.setTimeOffset(-10800);
+
+  GravarLogin(UniqueId(), WiFi.localIP().toString().c_str());
+}
+
+void loop() //------------------------------------LOOP-------------------------------------
+{
+  tensao = tensao +10;
+  corrente = corrente + 11;
+  leituraLDR = leituraLDR + 12;
+  
+ // Serial.println(RetornarHorario());
+  Serial.println(LerTENSAOSensor());
+  
+  EnviarJson(tensao, corrente, leituraLDR); // Enviar Json Serviço
+  GravarHorario(tensao, corrente, leituraLDR, RetornarHorario(), RetornarData(),UniqueId(), 1); // Gravar Hora Tabela
+ }
+  
+//----------------------------------------------------------------------------------------------------------------------EnviarJson
+
+void EnviarJson(float tensao, float corrente, float luminosidade)
+{
+  delay(500);
+  //Serial.print(F("Sending: "));
+  StaticJsonDocument<1000> doc2;
+   
+   WiFiClient client = server.available();
+   if (client) {
+    
+   // Write response headers
+
+   doc2["tensao"] = tensao;
+   doc2["corrente"] = corrente;
+   doc2["luminosidade"] = luminosidade;
+   doc2["horario"] = horario;
+   doc2["data"] = data_atual;
+  
+   //Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        Serial.write(c);
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println(F("Content-Type: application/json"));
+            client.println(measureJsonPretty(doc2));
+            client.println();
+            serializeJsonPretty(doc2, client);
+            break;
+          } else {
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+    }
+    client.stop();
+    Serial.println("Client Disconnected.");
+  }
+}
+//----------------------------------------------------------------------------------------------------------------------RetornarHorario
+String RetornarHorario()
+{
+   while(!timeClient.update()) 
+   {
+   timeClient.forceUpdate();
+   }
+  // The formattedDate comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+   formattedDate = timeClient.getFormattedDate();
+  // Extract date
+  int splitT = formattedDate.indexOf("T");
+  // Extract time
+  horario = formattedDate.substring(splitT+1, formattedDate.length()-1);
+
+  return horario;
+}
+
+//----------------------------------------------------------------------------------------------------------------------RetornarData
+String RetornarData()
+{
+  while(!timeClient.update()) 
+   {
+   timeClient.forceUpdate();
+   }
+  // The formattedDate comes with the following format:
+  // 2018-05-28T16:00:13Z
+  // We need to extract date and time
+   formattedDate = timeClient.getFormattedDate();
+
+  // Extract date
+  int splitT = formattedDate.indexOf("T");
+  data_atual = formattedDate.substring(0, splitT);
+
+  return data_atual;
+}
+//----------------------------------------------------------------------------------------------------------------------UniqueId
+String UniqueId()
+{
+
+  String UniqueId = "";
+  for(size_t i = 0; i < 6; i++)
+  {
+      UniqueId = UniqueId + String(UniqueID8[i], HEX);
+  }
+  return UniqueId;
+}
+//----------------------------------------------------------------------------------------------------------------------GravarLogin
+void GravarLogin(String uniqueId, String ip)
+{
+  const int httpPort = 80;
+  WiFiClient client3;
+
+  if (!client3.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    delay(1000);
+    return;
+  }
+
+  String url = "/login.php?";
+
+  url += "uniqueId=";
+  url +=  uniqueId;
+  url += "&ip=";
+  url +=  ip;
+
+  Serial.println("receiving from remote server");
+  Serial.println(url);
+  
+  client3.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+
+  unsigned long timeout = millis();
+  while (client3.available() == 0)
+  {
+    if (millis() - timeout > 5000 )
+    {
+      Serial.println (">>> Client Timeout |");
+      client3.stop();
+      return;
+    }
+  }
+
+  while (client3.available())
+  {
+    String line = client3.readStringUntil('\r');
+    Serial.print(line);
+
+    if (line.indexOf("salvo_com_sucesso") != -1)
+    {
+      Serial.println();
+      Serial.println("Atenção, dados salvos na tabela login!");
+    }
+  }
+  // Close the connection
+  Serial.println();
+  Serial.println("Conexao Fechada");
+  client3.stop();
+  //fim_mysql
+}
+
+//----------------------------------------------------------------------------------------------------------------------GRAVAR HORARIO
+
+void GravarHorario(float tensao, float corrente, float luminosidade, String horario, String data_atual, String UniqueId, int num)
+{
+   if (horario.compareTo("00:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("01:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("02:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("03:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual,UniqueId, num);
+  }
+  else if (horario.compareTo("05:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("06:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("07:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("08:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("09:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("10:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("11:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num); 
+  }
+  else if (horario.compareTo("12:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("13:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual,UniqueId, num);
+  }
+  else if (horario.compareTo("14:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("15:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("16:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("17:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual,UniqueId, num);
+  }
+  else if (horario.compareTo("18:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num); 
+  }
+  else if (horario.compareTo("19:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("20:00:00") == 0 )
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("21:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("22:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  }
+  else if (horario.compareTo("23:00:00") == 0)
+  {
+    GravarDadosMySql(tensao, corrente, luminosidade, horario, data_atual, UniqueId, num);
+  } 
+}
+
+//----------------------------------------------------------------------------------------------------------------------GravarDadosMySql
+
+void GravarDadosMySql(float tensao, float corrente, float luminosidade, String horario, String data_atual, String token, int flag)
+{ 
+  delay(1000);
+  const int httpPort = 80;
+  WiFiClient client2;
+
+  if (!client2.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    delay(1000);
+    return;
+  }
+
+  String url = "/salvar.php?";
+
+  url += "tensao=";
+  url +=  tensao;
+
+  url += "&corrente=";
+  url +=  corrente;
+
+  url += "&luminosidade=";
+  url +=  luminosidade;
+
+  url += "&horario=";
+  url +=  horario;
+
+  url += "&data_atual=";
+  url +=  data_atual;
+
+  url += "&token=";
+  url +=  token;
+
+  
+  Serial.println("receiving from remote server");
+  Serial.println(url);
+
+
+  client2.print(String("GET ") + url + " HTTP/1.1\r\n" +
+               "Host: " + host + "\r\n" +
+               "Connection: close\r\n\r\n");
+
+
+  unsigned long timeout = millis();
+  while (client2.available() == 0)
+  {
+    if (millis() - timeout > 5000 )
+    {
+      Serial.println (">>> Client Timeout |");
+      client2.stop();
+      return;
+    }
+  }
+  while (client2.available())
+  {
+    String line = client2.readStringUntil('\r');
+    Serial.print(line);
+
+    if (line.indexOf("salvo_com_sucesso") != -1 || line.indexOf(" "))
+    {
+      Serial.println();
+      Serial.println("Atenção, dados salvos com sucesso!");
+    }
+  }
+  // Close the connection
+  Serial.println();
+  Serial.println("Conexao Fechada");
+  client2.stop();
+  
+  delay(1000);
+ }
+
+ //SENSORES
+ //-------------------------------------------------------------------------------------------SENSOR LDR
+float LerLDRSensor()
+{
+
+  valorpot = analogRead(pinoSensorLDR);  
+  Serial.println(valorpot);
+  leituraLDR = map(valorpot, 0, 255, 0, 255); 
+  Serial.println(leituraLDR);
+  
+  return leituraLDR;
+}
+ //-------------------------------------------------------------------------------------------SENSOR TENSAO
+ 
+ float LerTENSAOSensor()
+{
+  // SENSOR TENSAO
+   leituraSensor = analogRead(pinoSensorTensao); //FAZ A LEITURA DO PINO ANALÓGICO E ARMAZENA NA VARIÁVEL O VALOR LIDO
+   tensaoEntrada = (leituraSensor * 5.0) / 1024.0; //VARIÁVEL RECEBE O RESULTADO DO CÁLCULO
+   tensaoMedida = tensaoEntrada / (valorR2/(valorR1+valorR2)); //VARIÁVEL RECEBE O VALOR DE TENSÃO DC MEDIDA PELO SENSOR
+
+   return tensaoMedida;
+}
+
+ //-------------------------------------------------------------------------------------------SENSOR CORRENTE
+ float LerCORRENTESensor()
+{
+
+}
+ 
